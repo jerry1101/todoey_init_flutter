cfd35afe06bb367af0a4501c9333fca40d14468f
jerry hung <jerry110160@gmail.com>
2020-06-13 19:36:23 -0400

    manage local state of check box

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
@@ -9,10 +11,32 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('tttttt1'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  const TaskCheckbox({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _TaskCheckboxState createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool _isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.orange[700],
+      value: _isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          _isChecked = newValue;
+        });
+      },
     );
   }
 }
