62ff83e50a78d0b2123660a023de033ef86d008a
jerry hung <jerry110160@gmail.com>
2020-06-13 17:29:11 -0400

    Add controllers in bottomsheet

--- lib/widgets/add_task_sheet.dart
+++ lib/widgets/add_task_sheet.dart
@@ -11,10 +11,30 @@ class AddTaskSheet extends StatelessWidget {
         decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30))),
-        child: Center(
-          child: Text('data'),
+                topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Add Task',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Colors.lightBlueAccent),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              FlatButton(
+                color: Colors.lightBlueAccent,
+                  onPressed: () {
+// todo
+                  },
+                  child: Text('Save'))
+            ],
+          ),
         ),
       ),
     );
